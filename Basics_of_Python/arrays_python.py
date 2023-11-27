aab4a6f3b787e6c5ae6502ee3446e52b02f5e6cb
Mukilan Krishnakumar <mukilan.git@gmail.com>
2023-11-27 19:26:42 +0530

    Array Methods

--- Basics_of_Python/arrays_python.py
+++ Basics_of_Python/arrays_python.py
@@ -19,3 +19,29 @@ print("Concat Array", t3)
 # Repition - Repeat the same array
 t4 = t1 * 3
 print("Repition ", t4)
+
+
+# Array Methods
+# append - Add to the end
+nums = array("f", [1.00, 1.14, 2.31])
+print("Before appending", nums)
+nums.append(2.54)
+print("After appending", nums)
+
+# extend - Add another array to the end
+nums_2 = array("f", [3.14, 4.28])
+nums.extend(nums_2)
+print("Extended nums", nums)
+
+# insert(pos, val) - Position and Value insertion
+nums.insert(0, 1.11111)
+print("After insertion", nums)
+
+# remove - remove value if present
+nums.remove(1.0)
+print("After removing", nums)
+
+
+# pop - value at positon is removed
+nums.pop(0)
+print("After popping", nums)
